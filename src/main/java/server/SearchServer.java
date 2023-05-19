9fbb679d47f97f2971d1e792dcbbdb741889497f
Margarita Dmitrieva <123968727+Muzaslivka@users.noreply.github.com>
2023-05-19 23:28:50 +0300

    Fixed "#"

--- src/main/java/server/SearchServer.java
+++ src/main/java/server/SearchServer.java
@@ -41,7 +41,7 @@ public class SearchServer {
                         StringBuilder sb = new StringBuilder();
                         for (PageEntry pageEntry : resultList) {
                             sb.append(gson.toJson(pageEntry));
-                            sb.append("#");
+                       
                         }
                         String answerServer = sb.toString();
                         out.println(answerServer);
